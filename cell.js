b308a84024c015577888867cc08eb925a505f57e
TaiebAlFalah <mohamedtaieb.alfalah@esprit.tn>
2017-02-13 18:03:35 +0100

    Calling cells and setting value on them.

--- cell.js
+++ cell.js
@@ -1,10 +1,22 @@
 
-function Cell() {
-    this.pos = createVector(random(width),random(height));
-    this.r=50;
-    this.c=color(random(100,255),0,random(100,255))
-    this.move=function () {
+function Cell(pos,c,r) {
+    this.pos = pos|| createVector(random(width),random(height));
+    this.r = r||40;
+    this.c = c||color(random(100,255),0,random(100,255));
 
+
+    this.clicked = function (x, y) {
+        var d = dist(this.pos.x, this.pos.y, x, y);//check distance
+        if (d < this.r) {
+            return true;
+        }
+        return false;
+    }
+
+
+    this.mitosis=function () {
+        var cell=new Cell(this.pos,this.r/2,this.c);//call with actual value
+        return cell;
     }
     this.move=function () {
       var vel=p5.Vector.random2D();
--- sketch.js
+++ sketch.js
@@ -1,11 +1,23 @@
-var cell;
+var cells=[];
 function setup() {
 createCanvas(400,400);
-cell=new Cell();
+cells.push (new Cell());//Cell=name of constructer ,cell individual cell, cells heya el arraycells.push (new Cell());
+    cells.push(new Cell())
 }
 
 function draw() {
   background(51);
-  cell.move();
-  cell.show();
+  for(var i=0;i<cells.length;i++){
+  cells[i].move();
+  cells[i].show();
+    }
+}
+
+function mousePressed() {
+
+    for(var i=0;i<cells.length;i++){
+       if (cells[i].clicked(mouseX,mouseY)){
+console.log("CLICKED")
+       }
+    }
 }
